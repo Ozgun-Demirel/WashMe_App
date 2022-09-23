06f65d9bb151def5949e476b8d5d6a4b2abda473
Ozgun-Demirel <Sukruozgundemirel@gmail.com>
2022-09-23 17:32:48 +0300

    Orientation Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'InterfaceFunc/MaterialColorCreator.dart';
@@ -49,6 +50,8 @@ Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool? showIntro = prefs.getBool("showIntro");
 
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  
   runApp(RoadEO_3(showIntro: showIntro,));
 }
 
